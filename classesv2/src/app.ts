6c0bdde3abcc66cf6293c496c425bde126c635e2
alejandroolmed59 <00097017@uca.edu.sv>
2022-01-10 19:54:42 -0600

    Abstract classes

--- classesv2/src/app.ts
+++ classesv2/src/app.ts
@@ -29,8 +29,27 @@ class ItDepartment extends Department {
 }
 class AccountingDepartment extends Department{
     public static numeroTel = '274324231';
+    private static instance: AccountingDepartment;
     private lastReport:string;
 
+    private constructor(id:number, private reports: string[]) {
+        super(id, "Accounting")
+        this.lastReport = reports[0]
+    }
+
+    static getInstance(){
+        if(AccountingDepartment.instance){
+            return AccountingDepartment.instance;
+        }else{
+            AccountingDepartment.instance = new AccountingDepartment(1,  ["Perdí la empresa en el bajon de btc", "Nos liquidaron","Porqué ese numero es negativo"]);
+            return AccountingDepartment.instance
+        }
+    }
+
+    addReport(report:string){
+        this.reports.push(report);
+        this.lastReport = report;
+    }
     get mostRecentReport(){
         if(this.lastReport){
             return this.lastReport
@@ -42,26 +61,22 @@ class AccountingDepartment extends Department{
         this.addReport(value);
     }
 
-    constructor(id:number, private reports: string[]) {
-        super(id, "Accounting")
-        this.lastReport = reports[0]
-    }
-    addReport(report:string){
-        this.reports.push(report);
-        this.lastReport = report;
-    }
     describe() {
         console.log(`Econo chill ${this.id}`);
     }
  
+
 }
  
-const accounting = new AccountingDepartment(1, ["Nos liquidaron","Porqué ese numero es negativo","Perdí la empresa en el bajon de btc"]);
+//const accounting = new AccountingDepartment(1, ["Nos liquidaron","Porqué ese numero es negativo","Perdí la empresa en el bajon de btc"]);
+const accounting = AccountingDepartment.getInstance();
 const engineering = new ItDepartment(2, ["Sosa","Arguello"]);
 
 
 engineering.addEmployee("Olme");
 accounting.addEmployee("Will Casas");
 
-engineering.describe()
-accounting.describe()
+//engineering.describe()
+//accounting.describe()
+accounting.mostRecentReport = "Btc a 22k lul"
+console.log(accounting.mostRecentReport)
