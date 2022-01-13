59784588960c17327922e0e219ce16ca429bfd4c
alejandroolmed59 <00097017@uca.edu.sv>
2022-01-13 00:35:17 -0600

    keyof , typeof , idexed access types , mapped types

--- classesv2/src/mappedTypes.ts
+++ classesv2/src/mappedTypes.ts
@@ -4,13 +4,36 @@ interface Rating {
   sabor: string
 }
 
-type FruitCount<T> = {
-  [key in Fruit]: T;
+type FruitCount<F extends keyof any, T> = {
+  [K in F]: T;
 }
 
-const fruits: FruitCount<Rating> = {
+const fruits: FruitCount<Fruit, Rating> = {
     orange: {estrellas:2, sabor:"acido"},
     apple: {estrellas:5, sabor:"dulce"},
     banana: {estrellas:1, sabor:"amargo"}
 };
 console.log(fruits);
+
+// EJ2.
+interface CatInfo {
+  age: number;
+  breed: string;
+}
+type CatName = "miffy" | "boris" | "mordred" | "Frodo";
+
+const cats: Record<CatName, CatInfo> = {
+  miffy: { age: 10, breed: "Persian" },
+  boris: { age: 5, breed: "Maine Coon" },
+  mordred: { age: 16, breed: "British Shorthair" },
+  Frodo: { age: 6, breed: "furry " }
+};
+
+
+function getProperty<Type, Key extends keyof Type>(obj: Type, key: Key) {
+  return obj[key];
+}
+
+let x = { a: 1, b: 2, c: 3, d: 4 };
+
+getProperty(x, 'd');
--- generics/src/app.ts
+++ generics/src/app.ts
@@ -20,6 +20,37 @@ function countAndDescribe<T extends Lengthy >(element: T) : [T,string]{
 
 console.log(countAndDescribe(['Hola Mundooo!']));
 
+// Removing the readonly in generics 
+type CreateMutable<Type> = {
+  -readonly [Property in keyof Type]: Type[Property]; //Its an indexed access type Type[Property]
+};
+ 
+type LockedAccount = {
+  readonly id: string;
+  readonly name: string;
+  readonly balance: number;
+};
+ 
+type UnlockedAccount = CreateMutable<LockedAccount>;
+
+const pruebis : UnlockedAccount = {
+  id: "",
+  name: "",
+  balance: 0
+}
+// Removes 'optional' attributes from a type's properties
+type Concrete<Type> = {
+  [Property in keyof Type]-?: Type[Property];
+};
+ 
+type MaybeUser = {
+  id: string;
+  name?: string;
+  age?: number;
+};
+ 
+type User = Concrete<MaybeUser>;
+
 // keyof in generics
 function extractProperty<T, U extends keyof T>(object: T, key:U){
     return object[key];
@@ -61,27 +92,65 @@ class DataStorage<T extends string | number | boolean> {
   numberStorage.addItem(90942321);
   numberStorage.removeItem(564342);
   console.log(numberStorage.getItems());
-  */
-type Person ={
-    firstName: string;
-    age:number;
-    dayofB:Date;
+*/
+
+//The omit default generic utility type
+type Person = {
+  firstName: string;
+  age: number;
+  dayofB: Date;
 }
 
-const perMuti : Omit<Person, "dayofB" | "age"> = {firstName:"ale"} 
+const perMuti: Omit<Person, "dayofB" | "age"> = { firstName: "ale" }
 console.log(perMuti);
 
+
+// Record utility type and mapped types
 interface CatInfo {
-    age: number;
-    breed: string;
-  }
+  age: number;
+  breed: string;
+}
 type CatName = "miffy" | "boris" | "mordred" | "Frodo";
-   
+
 const cats: Record<CatName, CatInfo> = {
-    miffy: { age: 10, breed: "Persian" },
-    boris: { age: 5, breed: "Maine Coon" },
-    mordred: { age: 16, breed: "British Shorthair" },
-    Frodo: {age:6, breed:"peludo "}
+  miffy: { age: 10, breed: "Persian" },
+  boris: { age: 5, breed: "Maine Coon" },
+  mordred: { age: 16, breed: "British Shorthair" },
+  Frodo: { age: 6, breed: "furry " }
 };
 
-cats.boris;
+
+
+//'keyof' keyword used in generic function
+function getProperty<Type, Key extends keyof Type>(obj: Type, key: Key) {
+  return obj[key];
+}
+
+let x = { a: 1, b: 2, c: 3, d: 4 };
+
+getProperty(x, 'd');
+
+//Indexed access types
+
+const Harold = {status:44,mood:"angry"}
+type HaroldType = typeof Harold["status"];
+
+const MyArray = [
+  { name: "Alice", age: 15 },
+  { name: "Bob", age: 23 },
+  { name: "Eve", age: 38 },
+];
+
+type Person2 = typeof MyArray[0]["name"]; 
+
+interface Animal {
+  live(): void;
+}
+interface Dog extends Animal {
+  woof: ()=>void;
+}
+
+const frosty : Dog = {
+  live: ()=>{console.log("vive")},
+  woof: ()=>{console.log("wooof woof 🐶");}
+}
